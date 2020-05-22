ff02a83ed0c508c79bc5a5ef953aaaf67e6e770e
Gabriel Barbosa Pereira <superxl8gbpaa@outlook.com>
2020-05-22 17:09:36 -0300

    Added invert function

--- header/code.hpp
+++ header/code.hpp
@@ -2,7 +2,10 @@
 #define _CODE_HPP_
 
 #include <string>
+#include <fstream>
 
 void code(std::string signature, std::string title, std::string language, std::string path);
+
+std::string invert(std::string text);
 
 #endif
--- source/code.cpp
+++ source/code.cpp
@@ -1,3 +1,23 @@
 #include "../header/code.hpp"
 
-void code(std::string signature, std::string title, std::string language, std::string path){}
+void code(std::string signature, std::string title, std::string language, std::string path){
+    signature = invert(signature);
+    title = invert(title);
+    language = invert(language);
+}
+
+std::string invert(std::string text){
+    std::string reverse;
+    size_t x{text.size() - 1};
+    while(true){
+        if(x > 0){
+            reverse += text[x];
+            x--;
+        }
+        else if(x == 0){
+            reverse += text[x];
+            break;
+        }
+    }
+    return reverse;
+}
