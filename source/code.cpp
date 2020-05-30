e464414b86af8214ea7643521c7ee37a9a547099
Gabriel Barbosa Pereira <superxl8gbpaa@outlook.com>
2020-05-30 00:05:36 -0300

    tests in the convert function

--- source/code.cpp
+++ source/code.cpp
@@ -37,15 +37,29 @@ std::string invert(std::string text){
 }
 
 void convert(std::string text){
-    std::map<std::string, std::string> con;
-    con["A"] = "0"; con["B"] = "1"; con["C"] = "2"; con["D"] = "3"; con["E"] = "4"; con["F"] = "5";
-    con["G"] = "6"; con["H"] = "7"; con["I"] = "8"; con["J"] = "9"; con["K"] = "10"; con["L"] = "11";
-    con["M"] = "12"; con["N"] = "13"; con["O"] = "14"; con["P"] = "15"; con["Q"] = "16"; con["R"] = "17";
-    con["S"] = "18"; con["T"] = "19"; con["U"] = "20"; con["V"] = "21"; con["W"] = "22"; con["X"] = "23";
-    con["Y"] = "24"; con["Z"] = "25";
-    con["a"] = "26"; con["b"] = "27"; con["c"] = "28"; con["d"] = "29"; con["e"] = "30"; con["f"] = "31";
-    con["g"] = "32"; con["h"] = "33"; con["i"] = "34"; con["j"] = "35"; con["k"] = "36"; con["l"] = "37";
-    con["m"] = "38"; con["n"] = "39"; con["o"] = "40"; con["p"] = "41"; con["q"] = "42"; con["r"] = "43";
-    con["s"] = "44"; con["t"] = "45"; con["u"] = "46"; con["v"] = "47"; con["w"] = "48"; con["x"] = "49";
-    con["y"] = "50"; con["z"] = "51";
+    std::map<char, std::string> con;
+    std::string coded;
+    size_t t{text.size() - 1};
+    int i{0};
+    con['A'] = "0"; con['B'] = "1"; con['C'] = "2"; con['D'] = "3"; con['E'] = "4"; con['F'] = "5";
+    con['G'] = "6"; con['H'] = "7"; con['I'] = "8"; con['J'] = "9"; con['K'] = "10"; con['L'] = "11";
+    con['M'] = "12"; con['N'] = "13"; con['O'] = "14"; con['P'] = "15"; con['Q'] = "16"; con['R'] = "17";
+    con['S'] = "18"; con['T'] = "19"; con['U'] = "20"; con['V'] = "21"; con['W'] = "22"; con['X'] = "23";
+    con['Y'] = "24"; con['Z'] = "25";
+    con['a'] = "26"; con['b'] = "27"; con['c'] = "28"; con['d'] = "29"; con['e'] = "30"; con['f'] = "31";
+    con['g'] = "32"; con['h'] = "33"; con['i'] = "34"; con['j'] = "35"; con['k'] = "36"; con['l'] = "37";
+    con['m'] = "38"; con['n'] = "39"; con['o'] = "40"; con['p'] = "41"; con['q'] = "42"; con['r'] = "43";
+    con['s'] = "44"; con['t'] = "45"; con['u'] = "46"; con['v'] = "47"; con['w'] = "48"; con['x'] = "49";
+    con['y'] = "50"; con['z'] = "51";
+    con['0'] = "52"; con['1'] = "53"; con['2'] = "54"; con['3'] = "55"; con['4'] = "56"; con['5'] = "57";
+    con['6'] = "58"; con['7'] = "59"; con['8'] = "60"; con['9'] = "61"; con[' '] = ";";
+    while(true){
+        if(con.find(text[i]) != con.end() && con.find(text[i]) != con.find(' ') && i != t){
+            coded += con.find(text[i])->second+".";
+        }
+        else if(con.find(text[i]) == con.find(' ')){
+            coded += con[' '];
+        }
+        i++;
+    }
 }
