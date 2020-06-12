1abd259225fb115f17d0c55f8610c325913d1065
Gabriel Barbosa Pereira <superxl8gbpaa@outlook.com>
2020-06-11 23:18:03 -0300

    The convert decode function ended

--- source/decode.cpp
+++ source/decode.cpp
@@ -36,7 +36,7 @@ std::string convert_decode(std::string txt){
     int t{0};
     std::string ret;
     std::map<std::string, std::string> dec;
-    dec[0] = "A"; dec["1"] = "B"; dec["2"] = "C"; dec["3"] = "D"; dec["4"] = "E"; dec["5"] = "F"; 
+    dec["0"] = "A"; dec["1"] = "B"; dec["2"] = "C"; dec["3"] = "D"; dec["4"] = "E"; dec["5"] = "F"; 
     dec["6"] = "G"; dec["7"] = "H"; dec["8"] = "I"; dec["9"] = "J"; dec["10"] = "K"; dec["11"] = "L";
     dec["12"] = "M"; dec["13"] = "N"; dec["14"] = "O"; dec["15"] = "P"; dec["16"] = "Q"; dec["17"] = "R";
     dec["18"] = "S"; dec["19"] = "T"; dec["20"] = "U"; dec["21"] = "V"; dec["22"] = "W"; dec["23"] = "X";
@@ -48,23 +48,26 @@ std::string convert_decode(std::string txt){
     dec["50"] = "y"; dec["51"] = "z";
     dec["52"] = "0"; dec["53"] = "1"; dec["54"] = "2"; dec["55"] = "3"; dec["56"] = "4"; dec["57"] = "5";
     dec["58"] = "6"; dec["59"] = "7"; dec["60"] = "8"; dec["61"] = "9"; dec[";"] = " ";
-    while(true){
+    do{
         if(txt[t] != '.'){
-            if(dec.find(std::string(txt[t])) == dec.find(";")){
-                ret += dec.find(std::string(txt[t]))->second;
+            if(txt[t] == '@'){
+                ret += "@";
             }
-            else if(txt[t + 1] != '.'){
-                ret += dec.find(std::string(txt[t]))->second+dec.find(std::string(txt[t + 1]))->second;
+            else if(txt[t] == ';'){
+                ret += " ";
+            }
+            else if(txt[t + 1] == '.' || txt[t + 1] == '@' || txt[t + 1] == ';' || t == txt.size() - 1){
+                ret += dec.find(std::string(1, txt[t]))->second;
+            }
+            else{
+                std::string a{std::string(1, txt[t])};
+                std::string b{std::string(1, txt[t + 1])};
+                std::string uni{a + b};
+                ret += dec.find(uni)->second;
                 t++;
             }
-            else if(txt[t + 1] == '.'){
-                ret += dec.find(std::string(txt[t]))->second;
-            }
-        }
-        if(t == txt.size() - 1){
-            break;
         }
         t++;
-    }
+    }while(t <= txt.size() - 1);
     return ret;
 }
